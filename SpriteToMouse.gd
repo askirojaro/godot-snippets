a4cd40315028ed6257fa2a208c6865aa0c7906d3
askirojaro <44616001+askirojaro@users.noreply.github.com>
2019-04-14 20:06:21 -0600

    Delete SpriteToMouse.gd

--- SpriteToMouse.gd
+++
@@ -1,9 +0,0 @@
-extends Node2D
-# set sprite to mouse cursor
-onready var logo = get_node("Logo")
-
-func _ready():
-	set_process(true)
-	
-func _process(delta):
-	logo.set_global_position(get_viewport().get_mouse_position())
